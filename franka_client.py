330803d609de54cdc46903d09c3210a2cd2371d4
LazyJazz <zijian.lv@hotmail.com>
2026-01-29 19:46:11 +0800

    Joystick client

--- franka_client.py
+++ franka_client.py
@@ -4,6 +4,8 @@ import numpy as np
 import asyncio
 import time
 
+import pygame
+
 class FrankaClient:
     def __init__(self, ip_address, port):
         self.uri = f"ws://{ip_address}:{port}"
@@ -28,18 +30,51 @@ class FrankaClient:
         await self.websocket.send(message)
 
 async def main():
-    client = FrankaClient("localhost", 8765)
+
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--ip", type=str, default="localhost", help="IP address")
+    parser.add_argument("--port", type=int, default=8765, help="Port number")
+    args = parser.parse_args()
+
+    client = FrankaClient(args.ip, args.port)
     last_tp = time.time()
     dur = 0.0
+
+    pygame.init()
+    pygame.joystick.init()
+
     while True:
         ee_pos, targ_pos = await client.get_pos()
         print(f"End-effector position: {ee_pos}, Target position: {targ_pos}")
         cur_tp = time.time()
         dur = cur_tp - last_tp
         last_tp = cur_tp
-        new_targ_pos = targ_pos + np.array([0.0, 0.0, 0.01]) * dur
+
+        move_vel = np.array([0.0, 0.0, 0.0])
+
+        if pygame.joystick.get_count() > 0:
+            joystick = pygame.joystick.Joystick(0)
+            joystick.init()
+            
+            pygame.event.pump()  # Process event queue
+            
+            axis_0 = joystick.get_axis(0)
+            axis_1 = joystick.get_axis(1)
+
+            axis_2 = (joystick.get_axis(2) + 1.0) * 0.5
+            axis_5 = (joystick.get_axis(5) + 1.0) * 0.5
+            move_vel[1] = axis_0 * 0.05  # Scale to max 0.1 m/s
+            move_vel[0] = axis_1 * 0.05  # Invert Y axis
+            move_vel[2] = (axis_5 - axis_2) * 0.05  # Scale to max 0.1 m/s
+
+              # if button 1 pressed, terminate
+            if joystick.get_button(1):
+                print("Exiting joystick control.")
+                break
+
+        new_targ_pos = targ_pos + move_vel * dur
         await client.set_pos(new_targ_pos)
-        time.sleep(0)
 
 if __name__ == "__main__":
     asyncio.run(main())
